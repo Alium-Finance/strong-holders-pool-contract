364f72ab580ed500040603b23fb6dd914ea06e22
gfijrb <gfijrb@yandex.ua>
2021-08-06 20:49:36 +0300

    add test contracts, ERC20Mock.sol, FarmMock.sol, SHPMock.sol

---
+++ contracts/test/FarmMock.sol
@@ -0,0 +1,33 @@
+/// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.4;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import '../IStrongHolder.sol';
+
+contract FarmMock {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    address public rewardToken;
+    address public shp;
+
+    constructor(
+        address _rewardToken,
+        address _shp
+    ) public {
+        rewardToken = _rewardToken;
+        shp = _shp;
+
+        IERC20(rewardToken).safeApprove(shp, type(uint256).max);
+    }
+
+    function deposit(uint256 _pid, uint256 _amount) external {
+        if (_amount >= 100_000) {
+//            IERC20(rewardToken).safeApprove(shp, type(uint256).max);
+            IStrongHolder(shp).lock(msg.sender, _amount);
+        }
+    }
+}
---
+++ contracts/test/SHPMock.sol
@@ -0,0 +1,33 @@
+/// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.4;
+
+//import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+//import "@openzeppelin/contracts/utils/Counters.sol";
+//import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+import "../IStrongHolder.sol";
+import "../StrongHolderPool.sol";
+
+contract SHPMock is StrongHolderPool {
+
+    constructor(address _aliumToken) StrongHolderPool(_aliumToken) {
+        for (uint i; i < 100; i++) {
+            _lock(address(uint160(i+1)), i+1 * 100_000);
+        }
+    }
+
+    function getAddress(uint256 _num) external pure returns (address) {
+        return address(uint160(_num));
+    }
+
+    function trustedLock(address _to, uint256 _amount) external override(StrongHolderPool) {
+        _lock(_to, _amount);
+    }
+
+    function withdrawTo(uint256 _poolId, address _to) external {
+        _withdraw(_poolId, _to);
+    }
+}
