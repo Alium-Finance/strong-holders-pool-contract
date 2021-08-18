82741fad84339722ecda24fd90424e1c90790722
gfijrb <gfijrb@yandex.ua>
2021-08-19 01:50:04 +0300

    add ERC165 checking for ERC1155 token, ERC1155 holding, initializer, SHP right modifier, withdraw, fix all internal function bugs, multi reward set

--- contracts/NFTRewardPool.sol
+++ contracts/NFTRewardPool.sol
@@ -3,57 +3,71 @@
 pragma solidity =0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "./interfaces/IAliumGaming1155.sol";
 import "./interfaces/INFTRewardPool.sol";
 
-contract NFTRewardPool is INFTRewardPool, Ownable {
+contract NFTRewardPool is INFTRewardPool, Ownable, IERC1155Receiver, ERC1155Holder {
 
     struct Reward {
         uint256 tokenId;
         uint256 amount;
     }
 
-    struct SetRewardInput {
-        uint256 position;
+    struct InputReward {
         Reward[] rewards;
     }
 
     bool public status;
 
     IAliumGaming1155 public rewardToken;
+    address public shp;
 
-    mapping (uint256 => Reward[]) private _rewards;
+    mapping (uint256 => Reward[]) internal _rewards;
     // pool id -> withdraw position -> counter
-    mapping (address => mapping (uint256 => uint256)) private _logs;
-    mapping (address => mapping (uint256 => uint256)) private _balances;
+    mapping (address => mapping (uint256 => uint256)) internal _logs;
+    mapping (address => mapping (uint256 => uint256)) internal _balances;
 
-    constructor(IAliumGaming1155 _rewardToken) {
-        require(address(_rewardToken) != address(0), "Reward zero address");
+    event Logged(address, uint);
+
+    function initialize(IAliumGaming1155 _rewardToken, address _shp) external onlyOwner {
+        require(
+            address(_rewardToken) != address(0), 
+            "Reward zero address"
+        );
+        require(_shp != address(0), "SHP zero address");
+
+        require(ERC165Checker.supportsERC165(address(_rewardToken)), "ERC165 unsupported token");
+        require(ERC165Checker.supportsInterface(address(_rewardToken), type(IAliumGaming1155).interfaceId), "ERC1155 unsupported token");
 
         rewardToken = _rewardToken;
+        shp = _shp;
     }
 
-    function log(address _caller, uint256 _withdrawPosition) external override {
+    function log(address _caller, uint256 _withdrawPosition) external override onlySHP {
         _logs[_caller][_withdrawPosition] += 1;
+        emit Logged(_caller, _withdrawPosition);
     }
 
+    // todo: check data clear
     function claim() external {
-//        uint tokenId;
-//        uint amount;
         Reward memory reward;
-        uint256[100] memory __logs = getLogs(msg.sender);
+        uint256[101] memory _userLogs = getLogs(msg.sender);
         for (uint i; i <= 100; i++) {
-            if (__logs[i] != 0) {
+            if (_userLogs[i] != 0) {
+                // clear log data
+                delete _logs[msg.sender][i];
                 uint ll = _rewards[i].length;
                 for (uint ii; ii < ll; ii++) {
                     reward = _rewards[i][ii];
-                    IAliumGaming1155(rewardToken).mint(
+                    rewardToken.mint(
                         address(this),
                         reward.tokenId,
                         reward.amount,
                         ""
                     );
-                    try IAliumGaming1155(rewardToken).safeTransferFrom(
+                    try rewardToken.safeTransferFrom(
                         address(this),
                         msg.sender,
                         reward.tokenId,
@@ -69,10 +83,26 @@ contract NFTRewardPool is INFTRewardPool, Ownable {
         }
     }
 
-    function withdraw(address _account, uint256 _tokenId) external {
-        uint balance = _balances[_account][_tokenId];
-        _balances[_account][_tokenId] -= balance;
-        IAliumGaming1155(rewardToken).safeTransferFrom(address(this), _account, _tokenId, balance, "");
+    function withdraw(uint256 _tokenId) external {
+        _withdraw(msg.sender, msg.sender, _tokenId, _balances[msg.sender][_tokenId]);
+    }
+
+    function withdrawTo(address _to, uint256 _tokenId, uint256 _tokenAmount) external {
+        _withdraw(msg.sender, _to, _tokenId, _tokenAmount);
+    }
+
+    function _withdraw(address _from, address _to, uint256 _tokenId, uint256 _tokenAmount) private {
+        uint balance = _balances[_from][_tokenId];
+
+        require(balance > 0, "Withdraw empty balance");
+        require(_tokenAmount >= balance, "Not enough token balance");
+
+        _balances[_from][_tokenId] -= _tokenAmount;
+        rewardToken.safeTransferFrom(address(this), _to, _tokenId, balance, "");
+    }
+
+    function getBalance(address _account, uint _position) external view returns (uint256) {
+        return _balances[_account][_position];
     }
 
     // call contract
@@ -80,7 +110,7 @@ contract NFTRewardPool is INFTRewardPool, Ownable {
         res = _logs[_account][_withdrawPosition];
     }
 
-    function getLogs(address _account) public view returns (uint256[100] memory res) {
+    function getLogs(address _account) public view returns (uint256[101] memory res) {
         uint l = 100;
         uint i = 1;
         for (i; i <= l; i++) {
@@ -92,18 +122,47 @@ contract NFTRewardPool is INFTRewardPool, Ownable {
         return _rewards[_withdrawPosition];
     }
 
+    // @notice Reward will be overwritten
     function setReward(
         uint256 _position,
-        Reward[] memory __rewards
-    ) external {
-        uint l = __rewards.length;
-        uint i;
+        Reward[] memory _rewardsList
+    ) external onlyOwner {
+        uint l = _rewardsList.length;
+        uint i = 0;
+        delete _rewards[_position];
         for (i; i < l; i++) {
-            _rewards[_position][i] = __rewards[i];
+            _rewards[_position].push(_rewardsList[i]);
         }
     }
 
-//    function setRewards(SetRewardInput[] memory _input) external view returns () {
-//        return ;
-//    }
+    function setRewards(uint256[] memory _positions, InputReward[] memory _rewardsLists) external onlyOwner {
+        uint l = _positions.length;
+
+        require(l == _rewardsLists.length, "Incorrect length input data");
+
+        uint i;
+        uint ll;
+        for (i; i < l; i++) {
+            if (_positions[i] > 100 || _positions[i] == 0) {
+                require(false, "Wrong position index set");
+            }
+        }
+
+        i = 0;
+        for (i; i < l; i++) {
+            if (_rewards[_positions[i]].length != 0) {
+                delete _rewards[_positions[i]];
+            }
+
+            ll = _rewardsLists[i].rewards.length;
+            for (uint ii; ii < ll; ii++) {
+                _rewards[_positions[i]].push(_rewardsLists[i].rewards[ii]);
+            }
+        }
+    }
+
+    modifier onlySHP {
+        require(msg.sender == shp, "Only SHP contract");
+        _;
+    }
 }
